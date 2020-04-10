3aa1ce9751be6c00a64d3c3d7f8b41f21e7f18fb
Neha Nelson <nehanelson@Nehas-MBP.hsd1.nj.comcast.net>
2020-04-10 19:11:07 -0400

    fixed billing page

--- Billing.js
+++ Billing.js
@@ -5,25 +5,27 @@ import './Billing.css';
 
 function Billing() {
     return (
-        <Form>
+        <div class="page">
+            <Form>
 
-            <h1> Your Current Bill is: $215.00</h1>
-            <Form.Group controlId="formGroupEmail">
-                <Form.Label>Credit Card</Form.Label>
-                <Form.Control type="email" placeholder="1234-1234-1234-1234" />
-            </Form.Group>
-            <Form.Group controlId="formGroupPassword">
-                <Form.Label>CVC</Form.Label>
-                <Form.Control type="password" placeholder="000" />
-            </Form.Group>
-            <Form.Group controlId="formGroupPassword">
-                <Form.Label>Exp date</Form.Label>
-                <Form.Control type="password" placeholder="01/00" />
-            </Form.Group>
-            <>
-                <Button as="input" type="submit" value="Pay Now!" />{' '}
-            </>
-        </Form>
+                <h1> Your Current Bill is: $215.00</h1>
+                <Form.Group controlId="formGroupEmail">
+                    <Form.Label>Credit Card</Form.Label>
+                    <Form.Control type="email" placeholder="1234-1234-1234-1234" />
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                    <Form.Label>CVC</Form.Label>
+                    <Form.Control type="password" placeholder="000" />
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                    <Form.Label>Exp date</Form.Label>
+                    <Form.Control type="password" placeholder="01/00" />
+                </Form.Group>
+                <>
+                    <Button as="input" type="submit" value="Pay Now!" />{' '}
+                </>
+            </Form>
+        </div>
     );
 
 }
