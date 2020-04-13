01cf07bdfbb804df1554a3f004a7c3faee110599
Neha Nelson <nehanelson@Nehas-MBP.hsd1.nj.comcast.net>
2020-04-13 19:36:43 -0400

    updated calendar

--- Billing.js
+++ Billing.js
@@ -2,31 +2,34 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Form';
 import './Billing.css';
+import axios from "axios";
+class Billing extends React.Component {
+    render() {
 
-function Billing() {
-    return (
-        <div class="page">
-            <Form>
+        return (
+            <div class="page" >
+                <Form>
 
-                <h1> Your Current Bill is: $215.00</h1>
-                <Form.Group controlId="formGroupEmail">
-                    <Form.Label>Credit Card</Form.Label>
-                    <Form.Control type="email" placeholder="1234-1234-1234-1234" />
-                </Form.Group>
-                <Form.Group controlId="formGroupPassword">
-                    <Form.Label>CVC</Form.Label>
-                    <Form.Control type="password" placeholder="000" />
-                </Form.Group>
-                <Form.Group controlId="formGroupPassword">
-                    <Form.Label>Exp date</Form.Label>
-                    <Form.Control type="password" placeholder="01/00" />
-                </Form.Group>
-                <>
-                    <Button as="input" type="submit" value="Pay Now!" />{' '}
-                </>
-            </Form>
-        </div>
-    );
+                    <h1> Your Current Bill is: $215.00</h1>
+                    <Form.Group controlId="formGroupEmail">
+                        <Form.Label>Credit Card</Form.Label>
+                        <Form.Control type="email" placeholder="1234-1234-1234-1234" />
+                    </Form.Group>
+                    <Form.Group controlId="formGroupPassword">
+                        <Form.Label>CVC</Form.Label>
+                        <Form.Control type="password" placeholder="000" />
+                    </Form.Group>
+                    <Form.Group controlId="formGroupPassword">
+                        <Form.Label>Exp date</Form.Label>
+                        <Form.Control type="password" placeholder="01/00" />
+                    </Form.Group>
+                    <>
+                        <Button as="input" type="submit" value="Pay Now!" />{' '}
+                    </>
+                </Form>
+            </div>
+        );
+    }
 
 }
 export default Billing;
--- Make.js
+++ Make.js
@@ -8,68 +8,226 @@ import '@mobiscroll/react/dist/css/mobiscroll.min.css';
 
 
 
-function MakeReservation() {
-    let fromMonday = [];
-    let fromSaturday = [];
-    let twoWeeks = [];
+const now = new Date();
+const currYear = now.getFullYear();
+const currMonth = now.getMonth();
+const currDay = now.getDate();
+const min = new Date(currYear, currMonth, currDay);
+const max = new Date(currYear, currMonth + 6, currDay);
+let firstload = true;
 
-    for (let i = 0; i < 7; i++) {
-        fromMonday.push(new Date(2018, 0, 8 + i));
-        fromSaturday.push(new Date(2018, 0, 6 + i));
+class Make extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            colors: [],
+            labels: [],
+            invalid: [],
+            valid: []
+        };
+
     }
 
-    for (let j = 0; j < 14; j++) {
-        twoWeeks.push(new Date(2018, 0, 8 + j));
+    onPageLoadingSingle = (event, inst) => {
+        this.getPrices(event.firstDay, function callback(bookings) {
+            inst.settings.labels = bookings.labels
+            inst.settings.invalid = bookings.invalid;
+            inst.redraw();
+        });
+    }
+
+    onPageLoadingMultiple = (event, inst) => {
+        this.getBookings(event.firstDay, function callback(bookings) {
+            inst.settings.labels = bookings.labels
+            inst.settings.invalid = bookings.invalid;
+            inst.redraw();
+        });
+    }
+
+    getPrices = (d, callback) => {
+        var invalid = [],
+            labels = [];
+
+        mobiscroll.util.getJson('https://trial.mobiscroll.com/getprices/?year=' + d.getFullYear() + '&month=' + d.getMonth(), (bookings) => {
+            for (var i = 0; i < bookings.length; ++i) {
+                var booking = bookings[i],
+                    d = new Date(booking.d);
+
+                if (booking.price > 0) {
+                    labels.push({
+                        d: d,
+                        text: '$' + booking.price,
+                        background: 'none',
+                        color: '#e1528f'
+                    });
+                } else {
+                    invalid.push(d);
+                }
+            }
+            callback({ labels: labels, invalid: invalid });
+        }, 'jsonp');
+    }
+
+    getBookings = (d, callback) => {
+        var invalid = [],
+            labels = [];
+
+        mobiscroll.util.getJson('https://trial.mobiscroll.com/getbookings/?year=' + d.getFullYear() + '&month=' + d.getMonth(), (bookings) => {
+            for (var i = 0; i < bookings.length; ++i) {
+                var booking = bookings[i],
+                    d = new Date(booking.d);
+
+                if (booking.nr > 0) {
+                    labels.push({
+                        d: d,
+                        text: booking.nr + ' SPOTS',
+                        background: 'none',
+                        color: '#e1528f'
+                    });
+                } else {
+                    invalid.push(d);
+                }
+            }
+            callback({ labels: labels, invalid: invalid });
+        }, 'jsonp');
+    }
+
+    onInit = (event, inst) => {
+        if (firstload) {
+            mobiscroll.util.getJson('https://trial.mobiscroll.com/getrecbookings/', (times) => {
+                // We are loading the available spots from a remote API. The data needs to be parsed and days need to be disabled.
+                // In addition to that we'll have to display the number of available spots in lables plus update the time picker to only allow the valid selections.
+                // The approach is to invalidate all times and override (make them valid) if that time slot is available for booking. (Think basketball court for two hours)
+                var labels = [],
+                    invalid = [],
+                    valid = [];
+
+                for (var i = 0; i < times.length; ++i) {
+                    var time = times[i];
+                    // set all times to invalid
+                    invalid = invalid.concat({ d: 'w' + i, start: '00:00', end: '23:59' })
+
+                    for (var j = 0; j < time.length; ++j) {
+                        var t = time[j];
+                        // override invalid values with valids
+                        valid = valid.concat({ d: 'w' + i, start: t, end: t })
+                    }
+
+                    if (time.length === 0) {
+                        // set day to invalid if there is no selectable time on that day
+                        invalid = invalid.concat('w' + [i]);
+                    } else {
+                        // add the number of selectable times to labels
+                        labels = labels.concat({ d: 'w' + i, text: time.length + ' SPOTS', background: 'none', color: '#e1528f' });
+                    }
+                }
+
+                firstload = false;
+
+                this.setState({
+                    labels: labels,
+                    invalid: invalid,
+                    valid: valid
+                });
+            }, 'jsonp');
+        }
+    }
+
+    onDayChange = (event, inst) => {
+        this.setState({ colors: [{ d: 'w' + event.date.getDay(), background: '#e1528f' }] });
+    }
+
+    render() {
         return (
-            <mobiscroll.Form>
-                <div className="mbsc-grid">
-                    <div className="mbsc-row">
-                        <div className="mbsc-col-sm-12 mbsc-col-md-4">
-                            <mobiscroll.FormGroup>
-                                <mobiscroll.FormGroupTitle>Mon-Sun</mobiscroll.FormGroupTitle>
-                                <mobiscroll.Calendar
-                                    selectType="week"
-                                    defaultValue={fromMonday}
-                                    firstSelectDay={1}
-                                    firstDay={1}
-                                    display="inline"
-                                    type="hidden"
-                                />
-                            </mobiscroll.FormGroup>
-                        </div>
-                        <div className="mbsc-col-sm-12 mbsc-col-md-4">
-                            <mobiscroll.FormGroup>
-                                <mobiscroll.FormGroupTitle>Sat-Fri</mobiscroll.FormGroupTitle>
-                                <mobiscroll.Calendar
-                                    selectType="week"
-                                    defaultValue={fromSaturday}
-                                    firstSelectDay={6}
-                                    firstDay={1}
-                                    display="inline"
-                                    type="hidden"
-                                />
-                            </mobiscroll.FormGroup>
-                        </div>
-                        <div className="mbsc-col-sm-12 mbsc-col-md-4">
-                            <mobiscroll.FormGroup>
-                                <mobiscroll.FormGroupTitle>Multiple</mobiscroll.FormGroupTitle>
-                                <mobiscroll.Calendar
-                                    selectType="week"
-                                    defaultValue={twoWeeks}
-                                    firstSelectDay={1}
-                                    firstDay={1}
-                                    select="multiple"
-                                    display="inline"
-                                    type="hidden"
-                                />
-                            </mobiscroll.FormGroup>
-                        </div>
-                    </div>
-                </div>
+            <mobiscroll.Form className="dms-calendar-booking">
+                <mobiscroll.FormGroup>
+                    <mobiscroll.FormGroupTitle>Single date & appointment booking</mobiscroll.FormGroupTitle>
+                    <mobiscroll.Calendar
+                        display="inline"
+                        type="hidden"
+                        controls={['calendar']}
+                        min={min}
+                        max={max}
+                        yearChange={false}
+                        responsive={{
+                            small: {
+                                months: 1
+                            },
+                            large: {
+                                months: 2
+                            }
+                        }}
+                        onPageLoading={this.onPageLoadingSingle}
+                    />
+                </mobiscroll.FormGroup>
+                <mobiscroll.FormGroup>
+                    <mobiscroll.FormGroupTitle>Booking multiple appointments</mobiscroll.FormGroupTitle>
+                    <mobiscroll.Calendar
+                        display="inline"
+                        type="hidden"
+                        controls={['calendar']}
+                        min={min}
+                        max={max}
+                        yearChange={false}
+                        responsive={{
+                            small: {
+                                months: 1
+                            },
+                            large: {
+                                months: 2
+                            }
+                        }}
+                        responsive={{
+                            large: {
+                                rows: 7,
+                                calendarWidth: 400
+                            }
+                        }}
+                        select="multiple"
+                        onPageLoading={this.onPageLoadingMultiple}
+                    />
+                </mobiscroll.FormGroup>
+                <mobiscroll.FormGroup>
+                    <mobiscroll.FormGroupTitle>Recurring booking & resource allocation</mobiscroll.FormGroupTitle>
+                    <mobiscroll.Calendar
+                        display="inline"
+                        type="hidden"
+                        controls={['calendar', 'time']}
+                        min={min}
+                        max={max}
+                        layout="fixed"
+                        calendarWidth={400}
+                        cssClass="dm-calendar-booking"
+                        yearChange={false}
+                        steps={{
+                            hour: 2,
+                            minute: 60
+                        }}
+                        responsive={{
+                            xsmall: {
+                                calendarWidth: undefined
+                            },
+                            medium: {
+                                rows: 7,
+                                circular: [false, false, false, true],
+                                calendarWidth: 400
+                            }
+                        }}
+                        touchUi={false}
+                        timeFormat="h A"
+                        colors={this.state.colors}
+                        labels={this.state.labels}
+                        invalid={this.state.invalid}
+                        valid={this.state.valid}
+                        onInit={this.onInit}
+                        onDayChange={this.onDayChange}
+                    />
+                </mobiscroll.FormGroup>
             </mobiscroll.Form>
 
 
         );
     }
 }
-export default MakeReservation;
+export default Make;
